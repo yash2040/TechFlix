5eaafd1fa52c499533ea565cd44e0bb90b75267f
Yash Khandelwal <68386161+yash2040@users.noreply.github.com>
2020-07-17 11:41:01 +0530

    Add files via upload

---
+++ Blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from.models import Post,BlogComment,Udvotes,Pviews
+admin.site.register((Post,BlogComment,Udvotes,Pviews))
+# Register your models here.
---
+++ templates/home/yash.js
@@ -0,0 +1,1 @@
+console.log("dfnlslnf");
